f0fd7ec2565083ea159749602142c1f30bb2b591
datsukan <s.datsukan@gmail.com>
2023-02-24 12:12:12 +0900

    storybookが動く状態になった（パーサーはまだ）

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,7 +10,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
+    "@storybook/preset-create-react-app",
   ],
   "framework": "@storybook/react",
   "core": {
--- src/component/Codeblock.stories.tsx
+++ src/component/Codeblock.stories.tsx
@@ -11,5 +11,5 @@ const Template: ComponentStory<typeof Codeblock> = (args) => <Codeblock {...args
 
 export const Normal = Template.bind({});
 Normal.args = {
-    children: [<div><div>hoge</div></div>]
+    children: [<div>hoge</div>]
 }
