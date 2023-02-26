808e20b63b423ded33f93e5ee80fee61bf08d476
datsukan <s.datsukan@gmail.com>
2023-02-26 17:03:11 +0900

    構文木解析の処理を元に戻す

--- src/plugin/remarkCustomDirective.ts
+++ src/plugin/remarkCustomDirective.ts
@@ -1,6 +1,7 @@
-import type { Node, Data } from "unist"
-import visit from "unist-util-visit"
-import h from "hastscript"
+// @ts-nocheck
+import type { Node } from "unist"
+import { visit } from "unist-util-visit"
+import { h } from "hastscript"
 
 export default function remarkCustomDirective() {
   return transformer
@@ -15,32 +16,11 @@ export default function remarkCustomDirective() {
         node.type === "containerDirective"
       ) {
         const data = node.data || (node.data = {})
-        const name = node.data["name"]
-        const attributes = node.data["attributes"]
-        if (typeof name !== "string" && typeof name !== "undefined") return
-        if (!isNodeData(attributes)) return
-
-        const hast = h(name, attributes)
+        const hast = h(node.name, node.attributes)
 
         data.hName = hast.tagName
         data.hProperties = hast.properties
       }
     }
   }
-}
-
-function isNodeData(attr: unknown): attr is Node<Data> {
-  if (
-    typeof attr !== "string"
-    && !Array.isArray(attr)
-    && typeof attr !== "undefined") return false
-
-  if (Array.isArray(attr)) {
-    if (attr.length === 0) return false
-    for (const att of attr) {
-      if (typeof att !== "string" && !(att instanceof Node)) return false
-    }
-  }
-
-  return true
 }
